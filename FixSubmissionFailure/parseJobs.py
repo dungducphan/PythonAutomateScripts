5c62a964f90f1fd6dbfb07425ca9325e06f91820
Dung Phan <brianp.dung@utexas.edu>
2017-09-11 17:56:10 -0500

    DM0d02

--- FixSubmissionFailure/parseJobs.py
+++ FixSubmissionFailure/parseJobs.py
@@ -10,7 +10,7 @@ errorRegex = re.compile(r'Error')
 filesRegex = re.compile(r'dm41 : (\d.\d{4}) -- ssq14 :   (\d.\d{4}) -- ssq24 :   (\d.\d{4}) -- fileIndex :  (\d) -- (\d{1,3})')
 
 # Read job.out file
-tempFile = open('./job.out', 'r')
+tempFile = open('./jobSubmit.out', 'r')
 
 previousLine = ''
 for line in tempFile:
@@ -24,8 +24,7 @@ for line in tempFile:
             process = subprocess.Popen(bashCommand, shell=True)
             bashCommand = "jobsub_submit --expected-lifetime=36h --memory=1500MB --role=Analysis --resource-provides=usage_model=DEDICATED,OPPORTUNISTIC -G minos -g file:///pnfs/minos/persistent/users/dphan/FeldmanCousinsAppearanceAnalysisDM0d004/NueFitStandard/analysisjob.sh " + filesSearch[0][0] + " " + filesSearch[0][1] + " " + filesSearch[0][2] + " " + filesSearch[0][3] + " " + filesSearch[0][4]
             process = subprocess.Popen(bashCommand, shell=True)
-            time.sleep(5);
-            # print(bashCommand)
+            time.sleep(10);
     previousLine = line
 
 # Clean up
